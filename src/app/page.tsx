ddb8f37b5b09e1ebefe46d2daac7948c91e10c36
whoisseth <utkarshseth2019@gmail.com>
2024-02-05 01:10:21 +0530

    game completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,197 @@
-import Image from "next/image";
+/** @format */
+"use client";
+
+import clsx from "clsx";
+import { useEffect, useState } from "react";
+import { twMerge } from "tailwind-merge";
+
+function getRandomXOrO() {
+  // Generate a random number (0 or 1)
+  const randomNum = Math.floor(Math.random() * 2);
+  // Return 'X' if randomNum is 0, otherwise return 'O'
+  return randomNum === 0 ? "X" : "O";
+}
+
+// Example usage:
+const randomSymbol = getRandomXOrO();
+
+type boxesNumberType = {
+  boxNo: number;
+  chance: "X" | "0" | null;
+};
 
 export default function Home() {
+  // const boxes = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const boxesNumber: boxesNumberType[] = [
+    { boxNo: 1, chance: null },
+    { boxNo: 2, chance: null },
+    { boxNo: 3, chance: null },
+    { boxNo: 4, chance: null },
+    { boxNo: 5, chance: null },
+    { boxNo: 6, chance: null },
+    { boxNo: 7, chance: null },
+    { boxNo: 8, chance: null },
+    { boxNo: 9, chance: null }
+  ];
+
+  const winingStates = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    //
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    //
+    [1, 5, 9],
+    [3, 5, 7]
+  ];
+
+  const [boxes, setBoxes] = useState<boxesNumberType[]>(boxesNumber);
+  const [currentChance, setCurrentChance] = useState(randomSymbol);
+  const [xChances, setXChances] = useState<number[]>([]);
+  const [ZeroChances, setZeroChances] = useState<number[]>([]);
+  const [win, setWin] = useState<"X" | "0" | "Draw" | null>(null);
+
+  function checkWinningCondition(array: number[]) {
+    for (const winState of winingStates) {
+      // Check if every element in winState is present in the array
+      if (winState.every((num) => array.includes(num))) {
+        return true; // If found, return true
+      }
+    }
+    return false; // If no winning state is found, return false
+  }
+
+  function handleReset() {
+    setCurrentChance(randomSymbol);
+    setXChances([]);
+    setZeroChances([]);
+    setWin(null);
+    setBoxes(boxesNumber);
+  }
+
+  useEffect(() => {
+    const xWin = checkWinningCondition(xChances);
+    const ZeroWin = checkWinningCondition(ZeroChances);
+    const allBoxesFill = boxes.every((d) => typeof d.chance == "string");
+
+    if (xWin) {
+      setWin("X");
+    }
+    if (ZeroWin) {
+      setWin("0");
+    }
+    if (!xWin && !ZeroWin && allBoxesFill) {
+      setWin("Draw");
+    }
+  }, [xChances, ZeroChances]);
+
+  function handleOnClickBox(boxNumber: number) {
+    // x chnace
+    if (currentChance === "X") {
+      const updatedBoxes: boxesNumberType[] = boxes.map((d) =>
+        d.boxNo == boxNumber ? { boxNo: boxNumber, chance: "X" } : d
+      );
+      setBoxes(updatedBoxes);
+
+      setXChances([...xChances, boxNumber]);
+      setCurrentChance("0");
+    }
+
+    // zero chnace
+    if (currentChance === "0") {
+      const updatedBoxes: boxesNumberType[] = boxes.map((d) =>
+        d.boxNo == boxNumber ? { boxNo: boxNumber, chance: "0" } : d
+      );
+      setBoxes(updatedBoxes);
+
+      setZeroChances([...ZeroChances, boxNumber]);
+      setCurrentChance("X");
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="min-h-screen flex items-center justify-center w-full bg-black text-white flex-col">
+      <main className="w-full  max-w-[400px] flex flex-col gap-4">
+        {/* chance */}
+        <section className="flex justify-between  ">
+          <p className="text-2xl">
+            <span>Chance : </span>
+            <span className="font-bold text-3xl text-blue-400">
+              {currentChance}
+            </span>
+          </p>
+          <button
+            onClick={handleReset}
+            className="bg-white text-black px-4 rounded font-semibold"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            Rematch
+          </button>
+        </section>
+
+        {/* boxes */}
+        <section className=" gap-2 grid grid-cols-3 w-full  max-w-[400px] h-[400px]  ">
+          {boxes.map((d, i) => (
+            <Box
+              win={win}
+              xChances={xChances}
+              ZeroChances={ZeroChances}
+              chance={d.chance}
+              boxNumber={d.boxNo}
+              handleOnClickBox={handleOnClickBox}
+              key={i}
             />
-          </a>
-        </div>
-      </div>
+          ))}
+        </section>
+        {/* winners */}
+        <section className="text-3xl">
+          {win === "0" && <p> 0 Win </p>}
+          {win === "X" && <p> X Win </p>}
+          {win === "Draw" && <p> Draw </p>}
+        </section>
+      </main>
+    </div>
+  );
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+function Box({
+  boxNumber,
+  handleOnClickBox,
+  xChances,
+  ZeroChances,
+  chance,
+  win
+}: {
+  boxNumber: number;
+  chance: "X" | "0" | null;
+  win: "X" | "0" | "Draw" | null;
+  xChances: number[];
+  ZeroChances: number[];
+  handleOnClickBox: (boxNumber: number) => void;
+}) {
+  function handleClick() {
+    handleOnClickBox(boxNumber);
+  }
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+  const btnDisabled =
+    xChances.includes(boxNumber) ||
+    ZeroChances.includes(boxNumber) ||
+    win !== null;
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+  return (
+    <button
+      disabled={btnDisabled}
+      onClick={handleClick}
+      className={twMerge(
+        clsx(
+          "min-w-32 min-h-32  rounded-md bg-white text-black  flex items-center justify-center text-4xl font-bold  border-4 border-transparent   ",
+          btnDisabled && "cursor-not-allowed",
+          chance == "X" && "text-blue-400  border-blue-400 "
+        )
+      )}
+    >
+      {chance}
+    </button>
   );
 }
